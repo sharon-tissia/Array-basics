4f9f469ecaa5569fbe55e6773e79f23166a4a44d
Sharon Tissia <sharontissia@gmail.com>
2023-02-15 13:00:32 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+var product = 80 * 12;
+console.log (product);
+
+
+var num = 900
+num++
+
+console.log(num);
+var product = 20 * 20;
+console.log (product);
+var quotient = 44 / 4
+console.log (quotient)
+remainder = 11 % 3
+console.log (remainder)
+var myName = 'sharon'
+console.log (myName)
+var ourStr = "i come first"
+ourStr += "i come second"
+console.log (ourStr)
+var myStr = "this is my first project."
+myStr += "this is my second project."
+console.log (myStr)
+var myName = "sharon"
+var myStr = " My name is" + myName + "and i am a female"
+console.log (myStr)
+var myName = "james";
+var myStr = " My name is " + myName + " and i am a woman "
+
+console.log (`My name is ${myName} and i am a woman` )
+
+const firstName = 'Jonathan'
+
+const str = `Hello ${firstName}, Welcome to Livestock Wealth`
+console.log(str);
+
+
+
+
+
