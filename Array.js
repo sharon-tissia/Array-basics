0402ef494ae77c7ba58cb207caf1679be923354c
Sharon Tissia <sharontissia@gmail.com>
2023-03-21 16:26:37 +0300

    Array.js

---
+++ Array.js
@@ -0,0 +1,10 @@
+var myArray = [["quincy", 89], ["sharon", 23]];
+console.log (myArray);
+var myArray = [30, 20, 35];
+var myData = myArray [0];
+console.log (myData);
+console.log (myData);
+function timesFive (num) {
+    return num * 5;
+}
+console.log (timesFive (9));
