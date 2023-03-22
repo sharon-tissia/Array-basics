a3d3e490d624868346be5ac7b600aff99416f776
Sharon Tissia <sharontissia@gmail.com>
2023-03-22 10:51:30 +0300

    Array.js

--- Array.js
+++ Array.js
@@ -11,9 +11,20 @@ console.log (timesFive (9));
 /* This function displays a shoping list
 */
 let shoppingList = ['cabbages','tealeaves', 'juice', 'bread'];
+shoppingList.slice
 console.log (shoppingList.lenght);
 /* an array that displays selected colours
 */
-let selectedColors = ['red', 'blue', 'yellow'];
-selectedColors [3] = 'violet'
-console.log (selectedColors);
+let selectedColors = ['red', 'blue', 'yellow', 'green'];
+/* adding items into an array
+*/
+let myColors = ['red', 'blue', 'yellow'];
+myColors.push ('amber');
+myColors.push ('green');
+console.log (myColors);
+/* removing items from an array
+*/
+let myNames = ['james','john', 'keri', 'keit'];
+myNames.pop ();
+myNames.pop ();
+console.log (myNames);
